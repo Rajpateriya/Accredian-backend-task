caae11caf9319d0ced7f922591199388deab4588
Rajpateriya <pateriyaraj52@gmail.com>
2024-07-07 12:51:18 +0530

    databse url

--- server.js
+++ server.js
@@ -13,3 +13,22 @@ app.use('/api/referrals', referralRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL; // Replace with your Render database connection URL
+
+const client = new Client({
+  connectionString: connectionString,
+});
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to PostgreSQL database');
+  } catch (error) {
+    console.error('Error connecting to database', error);
+  }
+}
+
+connectToDatabase();
